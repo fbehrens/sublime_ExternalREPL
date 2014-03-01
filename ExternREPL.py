e283b4b3c3554d4f9e1d3b04ae10c3f7e85179cd
Frank Behrens <fbehrens@gmail.com>
2014-03-01 12:23:40 +0100

    use Popen

--- ExternREPL.py
+++ ExternREPL.py
@@ -1,7 +1,7 @@
 import sublime, sublime_plugin
-import  subprocess
+from subprocess import Popen
 """
-ls
+echo "hi"
 """
 class ExternReplDo(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -9,12 +9,12 @@ class ExternReplDo(sublime_plugin.TextCommand):
     for region in self.view.sel():
         if region.empty():
             line = self.view.line(region)
-            line_contents = self.view.substr(line)
+            line_contents = self.view.substr(line).replace('"','\\"') + '\\n'
             line_below = sublime.Region(line.b+1)
             self.view.sel().clear()
             self.view.sel().add(line_below)
-            command = s.get("con_emu_c") + ' -GuiMacro:0 Paste(0,"' + line_contents + '\\n")'
-            return_code = subprocess.call(command)
+            command = s.get("con_emu_c") + ' -GuiMacro:0 Paste(0,"' + line_contents + '")'
+            return_code = Popen(command)
             print(return_code)
         # else:
         #     print("Not Implemente yet: " + self.view.substr(region))
