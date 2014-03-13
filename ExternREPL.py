cfb203016546d2075520f5b51a38a7287882df11
Frank Behrens <fbehrens@gmail.com>
2014-03-13 11:58:34 +0100

    qoute \ in test to \\ on command line

--- ExternREPL.py
+++ ExternREPL.py
@@ -5,27 +5,31 @@ for testing
 echo "hi"
 echo 'ho'
 1 + 1
+echo \\
 """
 class ExternReplDo(sublime_plugin.TextCommand):
     def repl_command(self, text):
         if sublime.platform() == 'windows':
             command = 'ConEmuC -GuiMacro:0 Paste(0,"' + text + '\\n")'
+            print("Command= " + command)
             return Popen(command)
         else:
             # Multiline with tmux needs multiple commands
             command = 'tmux send-keys -t repl "' + text + '" C-m'
+            print("Command= " + command)
             return Popen(command,shell=True)
 
     def run(self, edit):
         for region in self.view.sel():
             if region.empty():
                 line = self.view.line(region)
-                line_contents = self.view.substr(line).replace('"','\\"')
+                line_contents = self.view.substr(line)
+                quoted = line_contents.replace('\\','\\\\').replace('"','\\"')
+                # print( repr( quoted ))
                 line_below = sublime.Region(line.b+1)
                 self.view.sel().clear()
                 self.view.sel().add(line_below)
-                return_value = self.repl_command( line_contents )
-                print(return_value)
+                self.repl_command( quoted )
             # else:
         #     print("Not Implemente yet: " + self.view.substr(region))
 
