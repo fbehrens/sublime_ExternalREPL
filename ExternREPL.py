3653f3a5e13318ca274b2ecae3586c50f246a24a
Frank Behrens <fbehrens@gmail.com>
2014-06-04 13:56:46 +0200

    reloads a clojure file

--- ExternREPL.py
+++ ExternREPL.py
@@ -1,30 +1,29 @@
 import sublime, sublime_plugin
 from subprocess import Popen
 """
-for testing
 echo "hi"
 echo 'ho'
 1 + 1
 echo \\
 """
-class ExternReplDo(sublime_plugin.TextCommand):
-    def repl_command(self, text):
-        s = sublime.load_settings("ExternalRepl.last-run")
-        s.set("last-text", text)
-        # \ => \\ | " => \" | remove \n
-        quoted = text.replace('\\','\\\\')\
-                     .replace('"','\\"')\
-                     .replace('\n','')
-        if sublime.platform() == 'windows':
-            command = 'ConEmuC -GuiMacro:0 Paste(0,"' + quoted + '\\n")'
-            print("Command= " + command)
-            return Popen(command)
-        else:
-            # Multiline with tmux needs multiple commands
-            command = 'tmux send-keys -t repl "' + quoted + '" C-m'
-            print("Command= " + command)
-            return Popen(command,shell=True)
+def repl_command(text):
+    s = sublime.load_settings("ExternalRepl.last-run")
+    s.set("last-text", text)
+    # \ => \\ | " => \" | remove \n
+    quoted = text.replace('\\','\\\\')\
+                 .replace('"','\\"')\
+                 .replace('\n','')
+    if sublime.platform() == 'windows':
+        command = 'ConEmuC -GuiMacro:0 Paste(0,"' + quoted + '\\n")'
+        print("Command= " + command)
+        return Popen(command)
+    else:
+        # Multiline with tmux needs multiple commands
+        command = 'tmux send-keys -t repl "' + quoted + '" C-m'
+        print("Command= " + command)
+        return Popen(command,shell=True)
 
+class ExternReplDo(sublime_plugin.TextCommand):
     def run(self, edit):
         for region in self.view.sel():
             if region.empty():
@@ -34,9 +33,9 @@ class ExternReplDo(sublime_plugin.TextCommand):
                 line_below = sublime.Region(line.b+1)
                 self.view.sel().clear()
                 self.view.sel().add(line_below)
-                self.repl_command( line_contents )
+                repl_command( line_contents )
             else:
-                self.repl_command( self.view.substr(region) )
+                repl_command( self.view.substr(region) )
 
 class ExternReplRepeat(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -48,8 +47,33 @@ class ExternReplRepeat(sublime_plugin.TextCommand):
             print("Command = " + command)
             return Popen(command,shell=True)
 
-
 class ExternReplLast(sublime_plugin.TextCommand):
     def run(self, edit):
         s = sublime.load_settings("ExternalRepl.last-run")
-        ExternReplDo.repl_command(self, s.get("last-text"))
+        repl_command(s.get("last-text"))
+
+class ExternReplLoad(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("save")
+        _, path = folder_path( sublime.active_window().folders(), \
+                               self.view.file_name())
+        repl_command(clj_load(path))
+
+def folder_path(folders,path):
+    "split path according to current folders"
+    folder = [f for f in folders if path.startswith(f)][0]
+    return [folder, path[len(folder)+1:] ]
+
+def clj_load(path):
+    return '(load-file "' + path.replace("\\","/") + '")'
+
+class ExternReplTest(sublime_plugin.TextCommand):
+    # view.run_command("extern_repl_test")
+    def test(self, message, truthy):
+        if(not truthy):
+            print(message)
+
+    def run(self, edit):
+        self.test("folder_path", folder_path( ["a","b","c"], "b\\hh") \
+                                 == ["b","hh"])
+        print("ok")
--- readme.md
+++ readme.md
@@ -16,6 +16,7 @@ Works on Windows with ConEmu and on OSX/Linux with tmux
 * `ctrl-shift-enter`: sends current line to external REPL, cursor walks to next line 
 * `ctrl-shift-l(deactivated)`: sends last command to external REPL (Windows only)
 * `ctrl-shift-s`: sends up-arrow and return to external REPL
+* `ctrl-shift-l`: reloads a clojure file in external REPL (load_file "test/wppe_test.clj")
 
 ## ToDo
 - detection of blocks (ruby method definitions, clojure forms)
