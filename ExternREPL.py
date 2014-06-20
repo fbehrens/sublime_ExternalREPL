159f25eee24f97083c60ab0a893f03f0ac9cd612
Frank Behrens <fbehrens@gmail.com>
2014-06-20 13:12:31 +0200

    Run Powershell Tests and save last command

--- ExternREPL.py
+++ ExternREPL.py
@@ -6,9 +6,21 @@ echo 'ho'
 1 + 1
 echo \\
 """
-def repl_command(text):
+
+def append_to_history(text):
+    def remove_dublicates(seq):
+        "while retaining order"
+        seen = set()
+        seen_add = seen.add
+        return [ x for x in seq if x not in seen and not seen_add(x)]
     s = sublime.load_settings("ExternalRepl.last-run")
-    s.set("last-text", text)
+    history = s.get("history")
+    history.insert(0,text) # prepend
+    s.set("history", remove_dublicates(history)[:9])
+    sublime.save_settings("ExternalRepl.last-run")
+
+def repl_command(text):
+    append_to_history(text)
     # \ => \\ | " => \" | remove \n
     quoted = text.replace('\\','\\\\')\
                  .replace('"','\\"')\
@@ -47,17 +59,36 @@ class ExternReplRepeat(sublime_plugin.TextCommand):
             print("Command = " + command)
             return Popen(command,shell=True)
 
-class ExternReplLast(sublime_plugin.TextCommand):
-    def run(self, edit):
-        s = sublime.load_settings("ExternalRepl.last-run")
-        repl_command(s.get("last-text"))
-
 class ExternReplLoad(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.run_command("save")
         _, path = folder_path( sublime.active_window().folders(), \
                                self.view.file_name())
         repl_command(clj_load(path))
+
+class ExternReplTest(sublime_plugin.TextCommand):
+    "Runs the Current File"
+    def run(self, edit):
+        self.view.run_command("save")
+        _, path = folder_path( sublime.active_window().folders(), \
+                               self.view.file_name())
+        repl_command(powershell_test(path))
+
+class ExternReplTestOne(sublime_plugin.TextCommand):
+    "runs the selected tests"
+    def run(self, edit):
+        self.view.run_command("save")
+        _, path = folder_path( sublime.active_window().folders(), \
+                               self.view.file_name())
+        repl_command(powershell_test_one(path,selected_testnames(self)))
+
+class ExternReplHistory(sublime_plugin.TextCommand):
+    "runs the command from the history"
+    def run(self, edit):
+        self.history = sublime.load_settings("ExternalRepl.last-run").get("history")
+        self.view.window().show_quick_panel(self.history, self.select )
+    def select(self, x):
+        repl_command(self.history[x])
 
 def folder_path(folders,path):
     "split path according to current folders"
@@ -67,7 +98,38 @@ def folder_path(folders,path):
 def clj_load(path):
     return '(load-file "' + path.replace("\\","/") + '")'
 
-class ExternReplTest(sublime_plugin.TextCommand):
+def powershell_test(path):
+    return 'invoke-pester ' + path
+
+def powershell_test_one(path,testnames):
+    return 'invoke-pester ' + path + ' -testname ' + ' '.join(testnames)
+
+def selected_testnames(self):
+    "list of testnames"
+    acc = []
+    for region, test_name in region_testnames(self):
+        for s in self.view.sel():
+            if s.intersects(region):
+                acc.append( test_name )
+    return acc
+
+def region_testnames(self):
+    """does the pattern matching for "describe "" do"
+    return [ (region, 'test_name' ), ..  ]"""
+    patterns = {
+      "powershell": """^\s*describe\s+(?:'|")(.*)(?:'|")\s*\{\s*$""",
+      "ruby": """^\s*it\s+(?:'|")(.*)(?:'|")\s+do\s*$"""
+    }
+
+    test_names = []
+    regions = self.view.find_all(patterns["powershell"],fmt="$1",extractions=test_names)
+    regions[0].a  = 0
+    regions[-1].b = self.view.size()
+    for i,region in enumerate(regions[:-1]):
+        region.b = regions[i+1].a
+    return [ (region, test_names.pop(0)) for region in regions]
+
+class ExternReplTest1(sublime_plugin.TextCommand):
     # view.run_command("extern_repl_test")
     def test(self, message, truthy):
         if(not truthy):
@@ -76,4 +138,4 @@ class ExternReplTest(sublime_plugin.TextCommand):
     def run(self, edit):
         self.test("folder_path", folder_path( ["a","b","c"], "b\\hh") \
                                  == ["b","hh"])
-        print("ok")
+        print(1)
